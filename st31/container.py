69254311027d5347a216a02d7739222b94391a03
pavel1405 <ryzhikh.as1405@asugubkin.ru>
2018-11-05 11:20:55 +0300

    Update

--- st31/container.py
+++ st31/container.py
@@ -1,4 +1,7 @@
-from .bbook import *
+
+from book import book
+from bbook import bbook
+
 import pickle
 
 FILENAME = '31.txt'
@@ -8,50 +11,40 @@ class Container():
     def __init__(self):
         self.container = []
 
-    @property
+    
     def add_book(self):
         book = book()
-        book.input
+        book.input()
         self.container.append(book)
 
-    @property
+    
     def add_bbook(self):
-        book = bbook()
-        book.input
-        self.container.append(book)
+        bbook = bbook()
+        bbook.input()
+        self.container.append(bbook)
 
-    @property
-    def edit(self):
-        book_number = int(input('введите номер редактируемого издания в списке'))
-        self.container[book_number].input
-
-    @property
-    def delete(self):
-        book_number = int(input('введите номер удаляемого издания в списке'))
-        self.container.pop(book_number)
-
-    @property
+       
     def clear_container(self):
         answer = input('Вы действительно хотите все удалить?(да/нет)')
         if answer == 'да':
             self.container.clear()
 
-    @property
+    
     def show(self):
         for book in self.container:
-            book.print
+            book.print()
                        
-    @property
+    
     def write_in_file(self):
         with open(FILENAME, 'wb') as file:
             pickle.dump(self.container, file)
 
-    @property
+    
     def read_from_file(self):
         with open(FILENAME, 'rb') as file:
-            self.container += pickle.load(file)
+            self.container = pickle.load(file)
 
-a =  Container()
-                       
-            
+        self.show()
+
+
         

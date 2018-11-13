62f984bce05535415e621943e612edbe21b68165
Dmitry55395 <deorditse.d55395>
2018-11-13 20:45:51 +0300

    ДЕОР
    немного подправил

--- st13/main.py
+++ st13/main.py
@@ -1,38 +1,29 @@
-from .spisok import Spisok
-
-spisok = Spisok()
-
-menucompany = {"1": ("Внести руководителя отдела", spisok.insert_DataPersone),
-               "2": ("Внести новго сотрудника", spisok.insert_DluyaNewPersone),
-               "3": ("Изменить данные сотрудника", spisok.edit_personal),
-               "4": ("Удалить сотрудника из базы", spisok.delete_personal),
-               "5": ("Вывести список сотрудников", spisok.display_spisok),
-               "6": ("Звписать список сотрудников в файл", spisok.write_to_file),
-               "7": ("Подгрузить список сотрудников из файла", spisok.read_from_file),
-               "8": ("Очистить весь список", spisok.clear_spisok),
-               "9": ("Выйти из меню", "")
-               }
-
-
+from .Group import *
+import os
+group=Group()
+MENU = [
+        ["Добавить студента", group.AddStudent],
+        ["Добавить старшекурсника", group.AddUndergraduate],
+        ["Вывести список на экран", group.ShowList],
+        ["Записать список в файл", group.WriteToFile],
+        ["Считать список из файла", group.ReadFromFile],
+        ["Очистить список", group.ClearList],
+        ["Редактировать элемент", group.ChangeStudent],
+        ["Удалить элемент списка", group.DeleteStudent],
+        ["Выход"]
+    ]
 def main():
-            while True:
-                for i in menucompany:
-                    print(i,": ", menucompany[i][0])
-                try: 
-                            c=int(input("Введите номер пункта меню для начала работы приложения:"))        
-                            if c <= len(menucompany):
-                                if int(c) == 9:
-                                    break
-                                else:
-                                    menucompany[str(c)][1]()
-                            else:
-                                print("Число не может быть больше 9")
-                except ValueError:
-                    print("Вы не ввели номер пункта меню")
-
-
+    print("------------------------------")
+    for i, item in enumerate(MENU):
+        print("{0:2}. {1}".format(i, item[0]))
+    print("------------------------------")
+    opt = int(input("Ввод: "))
+    if opt != 8:
+        MENU[opt][1]()
+        main()
 
 if __name__ == "__main__":
+    try:
         main()
-            
-        
+    except:
+        print("Неверный ввод")
